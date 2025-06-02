10556db4bfdd391e5965118d963ace9c84624b0d
nethmalds <gddsnethmal@students.nsbm.ac.lk>
2025-06-02 21:18:58 +0530

    chore: integrate uploadthings with web

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,7 @@ const config: Config = {
   content: [
     './src/app/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
+    './node_modules/@uploadthing/react/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
