3ad72778f229ae4d02b6373e418f5981fca001f5
nethmalds <gddsnethmal@students.nsbm.ac.lk>
2025-06-05 12:56:23 +0530

    refactor: slight changes in codebase

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,11 +1,14 @@
+import LenisWrapper from "@/components/LenisWrapper";
 import { TabsSwitcher } from "@/components/TabsSwitcher";
 
 export default function EventsPage() {
 	return (
 		<div className="min-h-screen bg-background">
-			<div className="max-w-6xl mx-auto px-4 py-12">
-				<TabsSwitcher />
-			</div>
+			<LenisWrapper>
+				<div className="max-w-6xl mx-auto px-4 py-32">
+					<TabsSwitcher />
+				</div>
+			</LenisWrapper>
 		</div>
 	);
 }
