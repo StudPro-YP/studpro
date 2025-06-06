f77ed48c70b1d5ed38ac377fb61edeadade84dae
nethmalds <gddsnethmal@students.nsbm.ac.lk>
2025-06-07 02:45:36 +0530

    feat: implement the partners page

---
+++ src/app/partners/page.tsx
@@ -0,0 +1,14 @@
+import LenisWrapper from "@/components/LenisWrapper";
+import { PartnerTabsSwitcher } from "@/components/PartnerTabsSwitcher";
+
+export default function PartnersPage() {
+  return (
+    <div className="min-h-screen bg-background">
+      <LenisWrapper>
+        <div className="max-w-6xl mx-auto px-4 py-32">
+          <PartnerTabsSwitcher />
+        </div>
+      </LenisWrapper>
+    </div>
+  );
+}
--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,11 +5,10 @@ export const Header = () => {
         <nav className="fixed top-0 left-0 w-full z-50 h-16 px-16 flex justify-between items-center bg-gray-50">
             <Link href="/" className="flex items-center">
                 <img src="/images/strudpro-logo.png" alt="StudPro Logo" className="h-10" />
-            </Link>
-            <div className="flex gap-8">
+            </Link>            <div className="flex gap-8">
                 <Link href="/#about">About Us</Link>
                 <Link href="/events">Event Timeline</Link>
-                <Link href="/#partners">Partners</Link>
+                <Link href="/partners">Partners</Link>
             </div>
         </nav>
     );
