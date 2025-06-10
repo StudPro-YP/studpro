2cfde18b0eb03d3520908d8af2b9814705cd9b90
ashjay <ashwinie.ja@gmail.com>
2025-06-11 00:27:56 +0530

    fix: header logo

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -5,9 +5,10 @@ export const Header = () => {
     return (
         <nav className="fixed top-0 left-0 w-full z-50 h-16 px-16 flex justify-between items-center bg-gray-50">
             <Link href="/" className="flex items-center">
-                <Image src="/images/strudpro-logo.png" alt="StudPro Logo" width={40} height={40} className="h-10" />
-            </Link><div className="flex gap-8">
-                <Link href="/#about">About Us</Link>
+                <Image src="/images/strudpro-logo.png" alt="StudPro Logo" width={0} height={0} sizes="5vw" style={{width: "100%"}} className="h-8" />
+            </Link>
+            <div className="flex gap-8">
+                <Link href="/about-us">About Us</Link>
                 <Link href="/events">Event Timeline</Link>
                 <Link href="/partners">Partners</Link>
             </div>
