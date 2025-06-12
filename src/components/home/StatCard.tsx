816eb6f037fd5ce484afa3f3a2dd12c69fb0f56a
ashjay <ashwinie.ja@gmail.com>
2025-06-12 17:11:39 +0530

    feat: stats section

---
+++ src/components/home/StatCard.tsx
@@ -0,0 +1,26 @@
+import { StatsData } from "@/components/home/Stats";
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
+
+interface StatCardProps {
+    stat: StatsData,
+    cardStyle?: string,
+    textStyle?: string,
+}
+
+export const StatCard = ({stat, cardStyle, textStyle}: StatCardProps) => {
+    return (
+        <Card className={cn("flex flex-row w-64 p-8 border-2 border-white shadow-2xl", cardStyle)}>
+            <div className="flec items-center justify-center">
+            {stat.icon && (() => {
+                const LucideIcon = require("lucide-react")[stat.icon];
+                return LucideIcon ? <LucideIcon className={cn("w-12 h-12", textStyle)} /> : require("lucide-react")["badge"];
+            })()}
+            </div>
+            <div className="flex flex-col">
+                <h3 className={cn("text-2xl font-bold", textStyle)}>{stat.value}+</h3>
+                <p className={cn("text-md font-semibold uppercase", textStyle)}>{stat.type}</p>
+            </div>
+        </Card>
+    )
+}
--- src/components/home/Stats.tsx
+++ src/components/home/Stats.tsx
@@ -1,7 +1,32 @@
+import { StatCard } from "@/components/home/StatCard";
+
+export interface StatsData {
+	type: string;
+	value: number;
+	icon?: string;
+}
+
 export const Stats = () => {
+
+	const statsData: StatsData[] = [
+		{ type: 'Universities', value: 21, icon: 'GraduationCap' },
+		{ type: 'Registrants', value: 3000, icon: 'Users' },
+		{ type: 'Streams', value: 10, icon: 'LayoutTemplate' },
+		{type: 'Speakers', value: 16, icon: 'MicVocal' },
+	]
 	return (
-		<div className="h-screen flex justify-center items-center bg-primary">
-			stats
+		<div className="h-screen flex flex-col justify-center items-center gap-4 bg-primary">
+			<h3 className="text-2xl font-semibold text-white">We have conducted StudPro for many Years. We ended StudPro7.0 with massive numbers.</h3>
+			<div className="flex justify-center items-center gap-4 mt-8">
+				{statsData.map((stat, index) => (
+					<StatCard
+						key={index}
+						stat={stat}
+						cardStyle={index % 2 === 0 ? 'bg-transparent' : 'bg-white'}
+						textStyle={index % 2 === 0 ? 'text-white' : 'text-primary'} />
+				))}
+			</div>
+
 		</div>
 	);
 };
